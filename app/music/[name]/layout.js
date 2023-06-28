e9f7e4c3591330ff7261614accead59625caa7c6
itochs <halglobe0108@gmail.com>
2023-06-28 19:40:16 +0900

    動画埋め込み

--- app/music/[name]/layout.js
+++ app/music/[name]/layout.js
@@ -1,3 +1,3 @@
 export default function RootLayout({ children }) {
-  return <main className="min-h-screen p-12">{children}</main>;
+  return <main className="max-h-screen max-w-screen p-3">{children}</main>;
 }
--- app/music/[name]/page.js
+++ app/music/[name]/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import SideMenu from "@/components/SideMenu";
 import TimeSlider from "./{feature}/TimeSlider";
+import VideoManagerButtons from "./{feature}/VideoManagerButtons";
 
 export default function Home({ params: { name } }) {
   const music_name = decodeURI(name);
@@ -8,15 +9,29 @@ export default function Home({ params: { name } }) {
     <>
       <div className="flex flex-row items-center">
         <div className="w-11/12">
-          <p>{music_name}</p>
+          <div className="flex items-end">
+            <p>{music_name}</p>
+          </div>
         </div>
         <div className="w-1/12">
           <SideMenu />
         </div>
       </div>
-      <div className="p-3">
+      <div className="m-1 px-3">
         <TimeSlider max={300} />
       </div>
+      <div>
+        <VideoManagerButtons />
+      </div>
+      <iframe
+        // width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/lIfHd0bEDNQ"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </>
   );
 }
