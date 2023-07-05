9fb0d19292345edd89aea88277629981372a7d8e
itochs <halglobe0108@gmail.com>
2023-07-05 17:42:08 +0900

    完全一致かふりがなの一致で検索

--- app/{feature}/Search.jsx
+++ app/{feature}/Search.jsx
@@ -3,13 +3,33 @@ import MusicSearch from "@/components/Search";
 import { Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
+function hiraToKana(str) {
+  return str.replace(/[\u3041-\u3096]/g, function (match) {
+    var chr = match.charCodeAt(0) + 0x60;
+    return String.fromCharCode(chr);
+  });
+}
+
 export default function Search({ data }) {
+  const [musics, setMusics] = useState([]);
   function handleChange(e) {
-    console.log(e.target.value);
+    const searchStr = e.target.value;
+    const filtered =
+      searchStr == ""
+        ? []
+        : data.filter(
+            ({ name, ruby }) =>
+              name.indexOf(searchStr) !== -1 ||
+              hiraToKana(ruby).indexOf(hiraToKana(searchStr)) !== -1
+          );
+    setMusics(filtered);
   }
   return (
     <Box sx={{ bgcolor: "background.paper", p: 2, border: 1, borderRadius: 2 }}>
       <MusicSearch handleChange={handleChange} />
+      {musics.map(({ name }) => {
+        return <p key={name}>{name}</p>;
+      })}
     </Box>
   );
 }
