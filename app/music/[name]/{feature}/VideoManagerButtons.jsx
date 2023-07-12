5c27b65c725848ededf6a2447e18018347aed905
itochs <halglobe0108@gmail.com>
2023-07-12 17:19:21 +0900

    video play, pause button

--- app/music/[name]/{feature}/VideoManagerButtons.jsx
+++ app/music/[name]/{feature}/VideoManagerButtons.jsx
@@ -4,16 +4,29 @@ import { IconButton, Button } from "@mui/material";
 
 import SwapHorizRoundedIcon from "@mui/icons-material/SwapHorizRounded";
 import PlayCircleFilledWhiteRoundedIcon from "@mui/icons-material/PlayCircleFilledWhiteRounded";
+import YouTube, { YouTubePlayer } from "react-youtube";
 
-export default function VideoManagerButtons() {
+export default function VideoManagerButtons({ YTPlayer }) {
+  function videoPlayer(e) {
+    const state = YTPlayer.getPlayerState();
+    if (state == YouTube.PlayerState.PAUSED) {
+      YTPlayer.playVideo();
+    } else if (state == YouTube.PlayerState.PLAYING) {
+      YTPlayer.pauseVideo();
+    }
+  }
+
   return (
     <div>
+      {/* ミラー */}
       <IconButton>
         <SwapHorizRoundedIcon />
       </IconButton>
-      <IconButton>
+      {/* 再生停止 */}
+      <IconButton onClick={videoPlayer}>
         <PlayCircleFilledWhiteRoundedIcon />
       </IconButton>
+      {/*  */}
       <span>
         <Button variant="outlined">0.5</Button>
         <Button variant="outlined">0.75</Button>
--- app/music/[name]/{feature}/VideoPlayer.jsx
+++ app/music/[name]/{feature}/VideoPlayer.jsx
@@ -1,23 +1,33 @@
+"use client";
+import React, { useState } from "react";
 import TimeSlider from "./TimeSlider";
 import VideoManagerButtons from "./VideoManagerButtons";
+import YouTube, { YouTubePlayer } from "react-youtube";
 
 export default function VideoPlayer() {
+  const [YTPlayer, setYTPlayer] = useState(null);
+  function handleReady(e) {
+    setYTPlayer(e.target);
+    console.log(e.target);
+  }
   return (
     <>
       <div>
         <TimeSlider max={300} />
       </div>
       <div>
-        <VideoManagerButtons />
+        <VideoManagerButtons YTPlayer={YTPlayer} />
       </div>
-      <iframe
+      {/* url: https://www.youtube.com/embed/lIfHd0bEDNQ */}
+      <YouTube videoId="lIfHd0bEDNQ" onReady={handleReady} />
+      {/* <iframe
         // width="100%"
         height="100%"
         src="https://www.youtube.com/embed/lIfHd0bEDNQ"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
-      ></iframe>
+      ></iframe> */}
     </>
   );
 }
