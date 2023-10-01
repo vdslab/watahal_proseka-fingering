d4ead3bfe4414c5c2206792385915b13d6ab6fdc
itochs <halglobe0108@gmail.com>
2023-10-01 17:47:04 +0900

    svgの高さをscrollable部分に与える

--- app/music/[videoId]/{feature}/fingering/Chart.jsx
+++ app/music/[videoId]/{feature}/fingering/Chart.jsx
@@ -2,11 +2,13 @@ import ScrollableBox from "./ScrollableBox";
 import ChartContent from "./ChartContent";
 
 export default function Chart({ width, YTPlayer, height, left, right, maxY }) {
+  const svgHeight = 25000;
+
   return (
-    <ScrollableBox height={height} YTPlayer={YTPlayer}>
+    <ScrollableBox height={height} YTPlayer={YTPlayer} svgHeight={svgHeight}>
       <ChartContent
         width={width}
-        height={height}
+        height={svgHeight}
         left={left}
         right={right}
         maxY={maxY}
