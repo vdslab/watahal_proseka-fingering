df449e907cf946c91bd6cbc9da7320d30dd9ebd4
itochs <halglobe0108@gmail.com>
2023-10-11 01:48:13 +0900

    クリックされたときに要素がダブるのを防ぐ

--- app/{feature}/RelationVis.jsx
+++ app/{feature}/RelationVis.jsx
@@ -6,35 +6,15 @@ export default function Relationvis({ similarityData, setNodeId }) {
   const width = 600;
   const height = 600;
   const svgRef = useRef();
+  const links = similarityData.links.map((d) => ({
+    ...d,
+    value: d.value * 20,
+  }));
+  const nodes = similarityData.nodes.map((d) => ({ ...d }));
 
-  const { links, nodes, simulation } = useMemo(() => {
-    const links = similarityData.links.map((d) => ({
-      ...d,
-      value: d.value * 20,
-    }));
-    const nodes = similarityData.nodes.map((d) => ({ ...d }));
-
-    const simulation = d3
-      .forceSimulation(nodes)
-      .force(
-        "link",
-        d3.forceLink(links).id((d) => d.id)
-      )
-      .force(
-        "charge",
-        d3.forceManyBody().strength((d) => d.value)
-      )
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .force(
-        "collide",
-        d3.forceCollide((d) => 20)
-      );
-
-    return { links, nodes, simulation };
-  }, [similarityData]);
-
-  useEffect(() => {
+  const { simulation } = useMemo(() => {
     const svg = d3.select(svgRef.current);
+    svg.selectChildren().remove();
 
     const link = svg
       .append("g")
@@ -57,24 +37,36 @@ export default function Relationvis({ similarityData, setNodeId }) {
       })
       .attr("r", 5);
 
-    node.append("title").text(function (d) {
-      return d.id;
-    });
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force(
+        "link",
+        d3.forceLink(links).id((d) => d.id)
+      )
+      .force(
+        "charge",
+        d3.forceManyBody().strength((d) => d.value)
+      )
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .force(
+        "collide",
+        d3.forceCollide((d) => 20)
+      )
+      .on("tick", () => {
+        link
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+        node.append("title").text(function (d) {
+          return d.id;
+        });
 
-    simulation.on("tick", () => {
-      link
-        .attr("x1", (d) => d.source.x)
-        .attr("y1", (d) => d.source.y)
-        .attr("x2", (d) => d.target.x)
-        .attr("y2", (d) => d.target.y);
+        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+      });
 
-      node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-    });
-
-    return () => {
-      d3.selectAll(".chart > g").remove();
-    };
-  });
+    return { simulation };
+  }, [similarityData]);
 
   return (
     <svg
