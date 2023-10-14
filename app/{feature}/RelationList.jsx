1b0fe2cf4a4ffc5c5109ee8cb1376961d9e55786
itochs <halglobe0108@gmail.com>
2023-10-15 03:28:33 +0900

    画面遷移

--- app/{feature}/RelationList.jsx
+++ app/{feature}/RelationList.jsx
@@ -13,6 +13,7 @@ import path from "path";
 import useSWR from "swr";
 import LaunchIcon from "@mui/icons-material/Launch";
 import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const fetcher = (...args) => fetch(args).then((res) => res.json());
 
@@ -24,32 +25,33 @@ export default function RelationList({ nodeId }) {
   );
   const wrapperRef = useRef();
   const [height, setHeight] = useState();
+  const router = useRouter();
   useEffect(() => {
     setHeight(wrapperRef.current?.clientHeight);
   }, [wrapperRef.current, nodeId]);
 
-  if (nodeId === undefined || nodeId === null) return <div>select node</div>;
+  // if (nodeId === undefined || nodeId === null) return <div>select node</div>;
   if (error || error2) return <div>Failed to load</div>;
-  if (!musicListData) return <div>Loading...</div>;
-  if (!similarityData) return <div>Loading...</div>;
+  // if (!musicListData) return <div>Loading...</div>;
 
-  const music = musicListData.find((music) => music.id === nodeId);
-  similarityData.sort((a, b) => b.similarity - a.similarity);
+  const music = musicListData?.find((music) => music.id === nodeId);
+  similarityData?.sort((a, b) => b.similarity - a.similarity);
   const similarMusics = similarityData
-    .slice(0, 10)
-    .map(({ target }) => musicListData.find((music) => music.id === target));
+    ?.slice(0, 10)
+    ?.map(({ target }) => musicListData?.find((music) => music.id === target));
+  console.log(similarMusics);
 
   return (
     <Box bgcolor={"white"} height={"100%"} ref={wrapperRef} overflow={"auto"}>
       <List sx={{ maxHeight: height }}>
-        <ListSubheader>現在の曲</ListSubheader>
+        <ListSubheader>曲名</ListSubheader>
         <ListItem>{music?.name}</ListItem>
         <Divider />
         <ListSubheader>似ている曲</ListSubheader>
-        {similarMusics.map(({ id, name, videoId }) => (
+        {similarMusics?.map(({ id, name, videoId }) => (
           <ListItemButton
             key={id}
-            onClick={() => router.push(`/music/${videoId}?id=${target}`)}
+            onClick={() => router.push(`/music/${videoId}?id=${id}`)}
           >
             <ListItemIcon>
               <LaunchIcon />
