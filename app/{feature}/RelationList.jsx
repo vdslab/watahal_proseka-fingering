320f68027b381aacb70b1be6e94589bcd5d67164
itochs <halglobe0108@gmail.com>
2023-10-14 19:17:33 +0900

    類似度上位10件の表示

--- app/{feature}/RelationList.jsx
+++ app/{feature}/RelationList.jsx
@@ -4,16 +4,28 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(args).then((res) => res.json());
 
 export default function RelationList({ nodeId }) {
-  const { data, error } = useSWR("/api/musics", fetcher);
+  const { data: musicListData, error } = useSWR("/api/music", fetcher);
+  const { data: similarityData, error2 } = useSWR(
+    nodeId ? `/api/music/similarity/${nodeId}` : null,
+    fetcher
+  );
 
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
+  if (error || error2) return <div>Failed to load</div>;
+  if (!musicListData) return <div>Loading...</div>;
+  if (!similarityData) return <div>Loading...</div>;
 
-  const music = data.find((music) => music.id === nodeId);
+  const music = musicListData.find((music) => music.id === nodeId);
+  similarityData.sort((a, b) => b.similarity - a.similarity);
+  const similarMusics = similarityData
+    .slice(0, 10)
+    .map(({ target }) => musicListData.find((music) => music.id === target));
 
   return (
     <div>
-      <p>{music.name}</p>
+      <p>{music?.name}</p>
+      {similarMusics.map(({ id, name, videoId }) => (
+        <p key={id}>{name}</p>
+      ))}
     </div>
   );
 }
