baeca0638ced72e5dfaa1e928e94772381cda757
itochs <halglobe0108@gmail.com>
2023-11-14 01:18:58 +0900

    譜面の描画

--- app/music/[videoId]/_components/musicScore/MusicScore.jsx
+++ app/music/[videoId]/_components/musicScore/MusicScore.jsx
@@ -1,5 +1,10 @@
 "use client";
 import useSWR from "swr";
+import NoteScore from "../fingering/NoteScore";
+import LineSkeleton from "../fingering/LineSkeleton";
+import * as d3 from "d3";
+import { Box, Stack } from "@mui/material";
+
 const fetcher = (...args) => fetch(args).then((res) => res.json());
 
 export default function MusicScore({ id }) {
@@ -10,5 +15,55 @@ export default function MusicScore({ id }) {
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
 
-  return <div>{JSON.stringify(data)}</div>;
+  const width = 100;
+  const height = 600;
+
+  const ys = data.map(({ y }) => y);
+  // console.log(ys[ys.length - 1]);
+  const separateNumber = 5;
+  const scoreLength = Math.ceil(ys[ys.length - 1] / separateNumber);
+  const separetedScore = Array(scoreLength)
+    .fill()
+    .map((_, i) =>
+      data.filter(
+        ({ y }) => i * separateNumber <= y && y < (i + 1) * separateNumber
+      )
+    );
+  // console.log(separetedScore);
+  const xScale = d3.scaleLinear().domain([0, 12]).range([0, width]).nice(10);
+  const yScales = separetedScore.map((score) =>
+    d3
+      .scaleLinear()
+      .domain(d3.extent(score.map(({ y }) => y)))
+      .range([0, height])
+      .nice(10)
+  );
+  const widthScale = d3.scaleLinear().domain([0, 12]).range([0, width]).nice();
+
+  return (
+    <Box display={"flex"}>
+      {separetedScore.map((score, i) => {
+        console.log(i, score);
+        return (
+          <Box margin={1}>
+            <svg width={width} height={height}>
+              <rect x1={0} y1={0} width={width} height={height} fill="white" />
+              <g>
+                <LineSkeleton
+                  maxY={separateNumber}
+                  xScale={xScale}
+                  height={height}
+                />
+                <NoteScore
+                  score={score}
+                  scales={{ xScale, yScale: yScales[i], widthScale }}
+                  noteheight={4}
+                />
+              </g>
+            </svg>
+          </Box>
+        );
+      })}
+    </Box>
+  );
 }
