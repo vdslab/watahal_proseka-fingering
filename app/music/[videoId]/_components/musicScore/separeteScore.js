d013e0071088a169e1f640886a0caf6a26965c63
itochs <halglobe0108@gmail.com>
2023-11-17 00:59:19 +0900

    middleの部分が区間内にあったら描画させたい

--- app/music/[videoId]/_components/musicScore/separeteScore.js
+++ app/music/[videoId]/_components/musicScore/separeteScore.js
@@ -1,9 +1,13 @@
 export function separateScore(score, separateSectionNumber) {
+  const ys = score.map(({ y }) => y);
+  const scoreLength = Math.ceil(ys[ys.length - 1] / separateSectionNumber);
+
   const notHoldNotes = score.filter(({ type }) => type !== "hold");
   const holdnotes = score.filter(({ type }) => type === "hold");
 
   const notHoldNotesSeparated = separateNotHoldNotes(
     notHoldNotes,
+    scoreLength,
     separateSectionNumber
   );
 
@@ -12,20 +16,22 @@ export function separateScore(score, separateSectionNumber) {
   holdnoteGroups.sort((a, b) => a[0].y - b[0].y);
   const holdnotesSeparated = separeteHoldNotes(
     holdnoteGroups,
+    scoreLength,
     separateSectionNumber
   );
 
   const scoreSeparated = notHoldNotesSeparated.map((notHoldNotes, i) => {
-    const holdnotes = holdnotesSeparated[i];
+    const holdnotes = holdnotesSeparated[i] ?? [];
     return [...notHoldNotes, ...holdnotes];
   });
-
   return scoreSeparated;
 }
 
-function separateNotHoldNotes(notHoldNotes, separateSectionNumber) {
-  const ys = notHoldNotes.map(({ y }) => y);
-  const scoreLength = Math.ceil(ys[ys.length - 1] / separateSectionNumber);
+function separateNotHoldNotes(
+  notHoldNotes,
+  scoreLength,
+  separateSectionNumber
+) {
   const separetedNotHoldNotes = Array(scoreLength)
     .fill()
     .map((_, i) =>
@@ -49,45 +55,70 @@ function GoupingHoldNotesReducer(acc, cur) {
   return acc;
 }
 
-function separeteHoldNotes(holdnoteGroups, separateSectionNumber) {
-  const lastHoldGroup = holdnoteGroups[holdnoteGroups.length - 1];
-  const lastY = lastHoldGroup[lastHoldGroup.length - 1].y;
-  const scoreLength = Math.ceil(lastY / separateSectionNumber);
-  const separetedStartHoldNotes = Array(scoreLength)
-    .fill()
-    .map((_, i) =>
-      holdnoteGroups
-        .filter((holdnoteGroup) => {
-          const start = i * separateSectionNumber;
-          const end = (i + 1) * separateSectionNumber;
-          const startHoldY = holdnoteGroup[0].y;
-          const endHoldY = holdnoteGroup[holdnoteGroup.length - 1].y;
-          return yInRange(startHoldY, start, end);
-        })
-        .flat()
-    );
+function separeteHoldNotes(holdnoteGroups, scoreLength, separateSectionNumber) {
+  const separetedStartHoldNotes = separateHoldNotesByType(
+    "start",
+    scoreLength,
+    holdnoteGroups,
+    separateSectionNumber
+  );
 
-  const separetedEndHoldNotes = Array(scoreLength)
-    .fill()
-    .map((_, i) =>
-      holdnoteGroups
-        .filter((holdnoteGroup) => {
-          const start = i * separateSectionNumber;
-          const end = (i + 1) * separateSectionNumber;
-          const endHoldY = holdnoteGroup[holdnoteGroup.length - 1].y;
-          return yInRange(endHoldY, start, end);
-        })
-        .flat()
-    );
+  const separetedEndHoldNotes = separateHoldNotesByType(
+    "end",
+    scoreLength,
+    holdnoteGroups,
+    separateSectionNumber
+  );
+
+  const separetedMiddleHoldNotes = separateHoldNotesByType(
+    "middle",
+    scoreLength,
+    holdnoteGroups,
+    separateSectionNumber
+  );
 
   const separetedHoldNotes = separetedStartHoldNotes.map(
     (startHoldNotes, i) => {
-      const endHoldNotes = separetedEndHoldNotes[i];
-      const holdNotes = [...new Set([...startHoldNotes, ...endHoldNotes])];
+      const endHoldNotes = separetedEndHoldNotes[i] ?? [];
+      const middleHoldNotes = separetedMiddleHoldNotes[i] ?? [];
+      const holdNotes = [
+        ...new Set([...startHoldNotes, ...endHoldNotes, ...middleHoldNotes]),
+      ];
       holdNotes.sort((a, b) => a.y - b.y);
       return holdNotes;
     }
   );
+
+  return separetedHoldNotes;
+}
+
+function separateHoldNotesByType(
+  type,
+  scoreLength,
+  holdnoteGroups,
+  separateSectionNumber
+) {
+  const separetedHoldNotes = Array(scoreLength)
+    .fill()
+    .map((_, i) =>
+      holdnoteGroups
+        .filter((holdnoteGroup) => {
+          const start = i * separateSectionNumber;
+          const end = (i + 1) * separateSectionNumber;
+
+          if (type === "middle") {
+            const checks = holdnoteGroup
+              .filter((_, i) => 0 < i && i < holdnoteGroup.length - 1)
+              .some(({ y }) => yInRange(y, start, end));
+            return checks;
+          }
+
+          const targetIndex = type === "start" ? 0 : holdnoteGroup.length - 1;
+          const endHoldY = holdnoteGroup[targetIndex].y;
+          return yInRange(endHoldY, start, end);
+        })
+        .flat()
+    );
 
   return separetedHoldNotes;
 }
