f9a8dfbf841bd4466a55c4eaf88973c21b98d89b
itochs <halglobe0108@gmail.com>
2023-11-20 02:06:15 +0900

    間隔を開ける

--- app/_components/MusicList.jsx
+++ app/_components/MusicList.jsx
@@ -1,9 +1,14 @@
 "use client";
 import React, { useState } from "react";
 import MusicTable from "@/components/MusicTable";
+import { Box } from "@mui/material";
 
 export default function MusicList({ musics }) {
   const [showedData, setShowedData] = useState(musics);
 
-  return <MusicTable data={showedData} />;
+  return (
+    <Box marginTop={3}>
+      <MusicTable data={showedData} />
+    </Box>
+  );
 }
