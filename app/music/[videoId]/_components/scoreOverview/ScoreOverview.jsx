32523c05afe27b9ad84f8e60cd602b3a97c63d84
itochs <halglobe0108@gmail.com>
2023-11-27 21:09:03 +0900

    運指の全体像

---
+++ app/music/[videoId]/_components/scoreOverview/ScoreOverview.jsx
@@ -0,0 +1,58 @@
+import { Box, Chip, ListItem, Stack, Tooltip, Typography } from "@mui/material";
+import DoneIcon from "@mui/icons-material/Done";
+import { useState } from "react";
+import ComplexityMusicScore from "../complexityMusicScore/MusicScore";
+
+export default function ScoreOverview({ id }) {
+  const [chipData, setChipData] = useState([
+    {
+      label: "complexity",
+      selected: true,
+      description: "譜面の複雑さを色の濃淡で表示",
+    },
+    {
+      label: "fingering",
+      selected: false,
+      disabled: true,
+      description: "運指の表示",
+    },
+  ]);
+
+  return (
+    <>
+      <Typography variant="h4" gutterBottom>
+        譜面の全体像
+      </Typography>
+      <Box marginBottom={2}>
+        <Stack direction={"row"} width={"20%"}>
+          {chipData.map(({ label, selected, disabled, description }, index) => (
+            <ListItem key={index}>
+              <Tooltip title={description} arrow>
+                <span>
+                  <Chip
+                    label={label}
+                    variant={selected ? "filled" : "outlined"}
+                    color="primary"
+                    icon={selected ? <DoneIcon /> : <span />}
+                    onClick={() => {
+                      const newChipData = [...chipData];
+                      newChipData[index].selected =
+                        !newChipData[index].selected;
+                      setChipData(newChipData);
+                    }}
+                    disabled={disabled}
+                  />
+                </span>
+              </Tooltip>
+            </ListItem>
+          ))}
+        </Stack>
+      </Box>
+
+      <ComplexityMusicScore
+        id={id}
+        view={chipData.find(({ label }) => label === "complexity").selected}
+      />
+    </>
+  );
+}
