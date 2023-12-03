ab396746ef1f895752e4576fdd87435d7010f42f
itochs <halglobe0108@gmail.com>
2023-12-03 17:56:23 +0900

    chipの描画を別コンポーネントに分けた

--- app/music/[id]/_components/scoreOverview/ScoreOverview.jsx
+++ app/music/[id]/_components/scoreOverview/ScoreOverview.jsx
@@ -3,8 +3,40 @@ import DoneIcon from "@mui/icons-material/Done";
 import { useState } from "react";
 import MusicScore from "./complexityMusicScore/MusicScore";
 
+function StackRowChips({ chips, chipsState }) {
+  const [viewChips, setViewChips] = chipsState;
+  return (
+    <Stack
+      direction="row"
+      justifyContent="flex-start"
+      alignItems="center"
+      spacing={2}
+    >
+      {chips.map(({ label, selected, description }, index) => (
+        <Box key={index}>
+          <Tooltip title={description} arrow>
+            <span>
+              <Chip
+                label={label}
+                variant={selected ? "filled" : "outlined"}
+                color="primary"
+                icon={selected ? <DoneIcon /> : <span />}
+                onClick={() => {
+                  const newChipData = [...viewChips];
+                  newChipData[index].selected = !newChipData[index].selected;
+                  setViewChips(newChipData);
+                }}
+              />
+            </span>
+          </Tooltip>
+        </Box>
+      ))}
+    </Stack>
+  );
+}
+
 export default function ScoreOverview() {
-  const [chipData, setChipData] = useState([
+  const [viewChips, setViewChips] = useState([
     {
       label: "複雑さ",
       id: "complexity",
@@ -19,45 +51,42 @@ export default function ScoreOverview() {
     },
   ]);
 
+  const [filteringChips, setFilteringChips] = useState([
+    {
+      label: "ノーツの色を無くす",
+      id: "grayScale",
+      selected: true,
+      description: "ノーツの色をグレースケールする",
+    },
+  ]);
+
   return (
     <>
       <Typography variant="h4" gutterBottom>
         譜面の全体像
       </Typography>
-      <Box marginBottom={2}>
-        <Stack
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-          spacing={2}
-        >
-          {chipData.map(({ label, selected, description }, index) => (
-            <Box key={index}>
-              <Tooltip title={description} arrow>
-                <span>
-                  <Chip
-                    label={label}
-                    variant={selected ? "filled" : "outlined"}
-                    color="primary"
-                    icon={selected ? <DoneIcon /> : <span />}
-                    onClick={() => {
-                      const newChipData = [...chipData];
-                      newChipData[index].selected =
-                        !newChipData[index].selected;
-                      setChipData(newChipData);
-                    }}
-                  />
-                </span>
-              </Tooltip>
-            </Box>
-          ))}
-        </Stack>
-      </Box>
+      <Stack spacing={2}>
+        <StackRowChips
+          chips={viewChips}
+          chipsState={[viewChips, setViewChips]}
+        />
+        <StackRowChips
+          chips={filteringChips}
+          chipsState={[filteringChips, setFilteringChips]}
+        />
+      </Stack>
 
-      <MusicScore
-        view={chipData.find(({ id }) => id === "complexity").selected}
-        showFingering={chipData.find(({ id }) => id === "fingering").selected}
-      />
+      <Box paddingY={2}>
+        <MusicScore
+          view={viewChips.find(({ id }) => id === "complexity")?.selected}
+          showFingering={
+            viewChips.find(({ id }) => id === "fingering")?.selected
+          }
+          grayScaled={
+            filteringChips.find(({ id }) => id === "grayScale")?.selected
+          }
+        />
+      </Box>
     </>
   );
 }
