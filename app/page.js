03d1e774bccae558fa346ad135d0068df9e11c6c
itochs <halglobe0108@gmail.com>
2023-12-12 15:30:38 +0900

    使っていないものを削除

--- app/page.js
+++ app/page.js
@@ -3,21 +3,8 @@ import Header from "@/components/Header";
 import MainPage from "./_components/MainPage";
 import path from "path";
 import { readCSV, readJSON } from "./readFile";
-import getSimilarityData from "./_components/getSimilarityData";
 
-function Sort() {
-  return (
-    <form>
-      <select className="w-full">
-        <option>表示順の変更（現在何もできない）</option>
-        <option>難易度順</option>
-      </select>
-    </form>
-  );
-}
-
-export default async function Home({ searchParams: { id } }) {
-  const c = "min-h-screen flex-col items-center justify-between p-12";
+export default async function Home() {
   const publicDir = path.join(process.cwd(), "public");
 
   const jsonDir = path.join(publicDir, "json");
