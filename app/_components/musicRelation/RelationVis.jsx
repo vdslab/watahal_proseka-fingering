899d21e0b7088ab7bc788ffb884e0f41a35fc07f
itochs <halglobe0108@gmail.com>
2023-12-21 01:50:31 +0900

    データの取得

--- app/_components/musicRelation/RelationVis.jsx
+++ app/_components/musicRelation/RelationVis.jsx
@@ -2,13 +2,25 @@
 import { Box, Grid } from "@mui/material";
 import RelationList from "./RelationList";
 import RelationVisContent from "./RelationVisContent";
+import useSWR from "swr";
 
-export default function Relationvis({ similarityData, setNodeId, nodeId }) {
+export default function Relationvis({ setNodeId, nodeId }) {
+  const { data, error, isLoading } = useSWR("/api/graph", (url) =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.nodes[0]);
+        return data;
+      })
+  );
+  if (error) return <p>failed to load</p>;
+  if (isLoading) return <p>loading data...</p>;
+
   return (
     <Grid container justifyContent={"space-between"} spacing={3}>
       <Grid item xs={7}>
         <RelationVisContent
-          similarityData={similarityData}
+          similarityData={data}
           setNodeId={setNodeId}
           nodeId={nodeId}
         />
