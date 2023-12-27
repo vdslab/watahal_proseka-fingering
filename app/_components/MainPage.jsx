6889c5ae76b8a010c575c64df1361044cc0bcccd
itochs <halglobe0108@gmail.com>
2023-12-27 18:21:09 +0900

    shadow

--- app/_components/MainPage.jsx
+++ app/_components/MainPage.jsx
@@ -61,7 +61,12 @@ export default function MainPage() {
                 selectedMusicId={selectedMusicId}
               />
             </Box>
-            <Box width={"100%"} height={"100%"} boxSizing={"border-box"}>
+            <Box
+              width={"100%"}
+              height={"100%"}
+              boxSizing={"border-box"}
+              boxShadow={1}
+            >
               <Relationvis
                 setNodeId={setSelectedMusicId}
                 nodeId={selectedMusicId}
